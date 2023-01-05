67fbc42e8617cd07b4e221c6b371a86a8e662185
tomohiko.sugiyama <tomohiko.sugiyama@sprocket.bz>
2023-01-05 09:35:24 +0900

    feat: use session

--- src/baseClient/baseClient.go
+++ src/baseClient/baseClient.go
@@ -3,23 +3,79 @@ package baseClient
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
 	"net/http"
 )
 
 const (
-	DefaultUrl = "https://www.toggl.com/api/v8"
+	DefaultAuthPassword = "api_token"
+	DefaultUrl          = "https://api.track.toggl.com/api/v9"
+	SessionCookieName   = "__Host-timer-session"
 )
 
 type TogglClient struct {
-	client *http.Client
-	// cookie *http.Cookie
+	client   *http.Client
+	Url      string
+	password string
+	cookie   *http.Cookie
 }
 
-func NewClient() (*TogglClient, error) {
-	return &TogglClient{
-		client: http.DefaultClient,
-	}, nil
+func NewClient(apiKey string) (*TogglClient, error) {
+	client := &TogglClient{
+		client:   http.DefaultClient,
+		Url:      DefaultUrl,
+		password: DefaultAuthPassword,
+	}
+
+	if len(apiKey) < 1 {
+		fmt.Printf("%s\n", "valid token required")
+		return nil, errors.New("token required")
+	}
+
+	if _, err := client.authenticate(apiKey); err != nil {
+		return nil, err
+	}
+	return client, nil
+}
+
+func (c *TogglClient) authenticate(apiKey string) (_ []byte, err error) {
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/%s", c.Url, "me/sessions"), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.SetBasicAuth(apiKey, DefaultAuthPassword)
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode >= 400 {
+		defer func() {
+			err = resp.Body.Close()
+		}()
+		b, _ := io.ReadAll(resp.Body)
+		return nil, errors.New(string(b))
+	}
+
+	hasSessionCookie := false
+	for _, value := range resp.Cookies() {
+		if value.Name == SessionCookieName {
+			fmt.Printf("Cookie: %s\n", value)
+			fmt.Printf("Setting Cookie\n")
+			c.cookie = value
+			hasSessionCookie = true
+			break
+		}
+	}
+
+	if !hasSessionCookie {
+		return nil, fmt.Errorf("auth cookie %q not found on authentication response", SessionCookieName)
+	}
+
+	return nil, err
 }
 
 func request(c *TogglClient, method, endpoint string, b interface{}) (_ *json.RawMessage, err error) {
@@ -36,9 +92,8 @@ func request(c *TogglClient, method, endpoint string, b interface{}) (_ *json.Ra
 	if err != nil {
 		return nil, err
 	}
-	// req.AddCookie(c.cookie)
+	req.AddCookie(c.cookie)
 	req.Header.Set("Content-Type", "application/json; charset=utf-8")
-	req.SetBasicAuth("apiKey", "api_token")
 	resp, err := c.client.Do(req)
 	if err != nil {
 		return nil, err
--- src/main.go
+++ src/main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"go-toggl-api-client/src/baseClient"
 )
 
 func main() {
-	client, err := baseClient.NewClient()
+	apiKey := flag.String("ak", "", "api key")
+	flag.Parse()
+	if len(*apiKey) == 0  {
+		fmt.Println("api key is nothing")
+		return
+	}
+
+	client, err := baseClient.NewClient(*apiKey)
 	if err != nil {
+		fmt.Println(err)
 		return
 	}
 	body, err := client.GetRequest("https://api.track.toggl.com/api/v9/me/time_entries")
 	if err != nil {
+		fmt.Println(err)
 		return
 	}
 	fmt.Print(string(*body))
